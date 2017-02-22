37dbfc1a360dbd222c000a017d012192c8ae077a
Thomas John Gulisao <thomas@syntacticsinc.com>
2017-02-22 12:58:20 +0800

    So, it begins

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,6 @@
+<nav class="tabs is-boxed">
+	<ul>
+		<router-link tag="li" active-class="is-active" to="/" exact><a>Home</a></router-link>
+		<router-link tag="li" active-class="is-active" to="/about"><a>About</a></router-link>
+	</ul>
+</nav>
